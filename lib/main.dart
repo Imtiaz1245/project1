dedd6c49e58aac0ced2ec2e1155d3b8bb71f0d10
Muhammad Imtiaz <87941174+Imtiaz1245@users.noreply.github.com>
2022-03-14 18:48:45 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NewClass(),
+      home: NewClass(jgkjdfgjasjcfffxkgjvxcvzkjxvzndxjkcvhszvxkjjndkj  ckcbfxkjvzxvszxcvjkxzkjbv xzjkvzxkjvxz  xcvxz),
     );
   }
 }
